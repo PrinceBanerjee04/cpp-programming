7a73d9e86f077170d9a76c9f8874334d9e8a0be0
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-14 11:06:15 +0530

    Create automorphic.cpp

---
+++ NUMBERS/automorphic.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int n1=n,d,c=0,sq=n*n;
+    while(n1>0)
+    {
+        n1/=10;
+        c++;
+    }
+    sq=n*n;
+    d=sq%(int)pow(10,c);
+    if(d==n)
+    cout<<"AUTOMORPHIC NUMBER";
+    else
+    cout<<"NOT A AUTOMORPHIC NUMBER";
+}
