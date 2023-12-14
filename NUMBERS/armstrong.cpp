8960b22df4f68ad88be463e9debe849d8a819652
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-14 11:04:02 +0530

    Create armstrong.cpp

---
+++ NUMBERS/armstrong.cpp
@@ -0,0 +1,17 @@
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int n1=n,d,c,s=0;
+    while(n1>0)
+    {
+        d=n1%10;
+        s=s+(d*d*d);
+        n1/=10;
+    }
+    if(s==n)
+    cout<<"ARMSTRONG NUMBER";
+    else
+    cout<<"NOT A ARMSTRONG NUMBER";
+}
