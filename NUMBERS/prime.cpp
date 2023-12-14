90e57919b389a91cc606f84fb42e5692d1e515b2
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-14 11:03:19 +0530

    Create prime.cpp

---
+++ NUMBERS/prime.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    int n, c = 0;
+    cout<<"Enter a number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            c++;
+    }
+    if (c == 2)
+        cout << "PRIME";
+    else
+        cout << "NOT PRIME";
+    return 0;
+}
