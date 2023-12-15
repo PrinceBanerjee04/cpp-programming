0fa7cbf4669507ada927cbc92ba0ed1f4c4ae3f6
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-15 13:11:10 +0530

    Create strong.c

---
+++ NUMBERS/strong.c
@@ -0,0 +1,22 @@
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int n1=n,d,f=1,s=0;
+    while(n1>0)
+    {
+        f=1;
+        d=n1%10;
+        for(int i=1;i<=d;i++)
+        {
+            f=f*i;
+        }
+        s=s+f;
+        n1/=10;
+    }
+    if(s==n)
+    cout<<"STRONG NUMBER";
+    else
+    cout<<"NOT A STRONG NUMBER";
+}
