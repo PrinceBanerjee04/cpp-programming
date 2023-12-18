ed38b50d9c8c4ba0eeef9f4f411fc397c68eaf92
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-18 17:08:52 +0530

    Create diamond.cpp

---
+++ PATTERN/diamond.cpp
@@ -0,0 +1,36 @@
+int main()
+{
+    int i, j,n,k;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    k=n-1;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1; j<=k; j++)
+        {
+            cout<<" ";
+        }
+        k--;
+        for(j=1; j<=(2*i-1); j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    k = 1;
+    for(i=1; i<=(n-1); i++)
+    {
+        for(j=1; j<=k; j++)
+        {
+            cout<<" ";
+        }
+        k++;
+        for(j=1; j<=(2*(n-i)-1); j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
