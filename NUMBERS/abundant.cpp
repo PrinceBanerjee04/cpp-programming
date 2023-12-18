475e84b8d4a4914d085b190c3fd9384ea1ad0ee7
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-18 17:07:22 +0530

    Create abundant.cpp

---
+++ NUMBERS/abundant.cpp
@@ -0,0 +1,19 @@
+int main()
+{
+    int n,s=0;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=1;i<n;i++)
+    {
+        if(n%i==0)
+        s=s+i;
+    }
+    if(s>n)
+    {
+        cout<<"ABUNDANT NUMBER"<<endl;
+        cout<<"ABUNDANCE IS: "<<s-n;
+    }
+    else
+    cout<<"NOT A ABUNDANT NUMBER";
+    return 0;
+}
