032457c8144f674084a57c255854b118fb5f310c
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-20 19:14:59 +0530

    Create bubble_sort.cpp

---
+++ ARRAY/bubble_sort.cpp
@@ -0,0 +1,36 @@
+int main()
+{
+    int n, i, j, min, temp;
+    cout << "Enter size : ";
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+    cout << "GIVEN ARRAY : ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout << "SOTED ARRAY : ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
