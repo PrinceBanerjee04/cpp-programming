d466e4942f4a9811938459e3c36bdfb8566a90bc
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-20 19:10:30 +0530

    Create linear_search.cpp

---
+++ ARRAY/linear_search.cpp
@@ -0,0 +1,28 @@
+int main()
+{
+    int n;
+    cout << "Enter size : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " ";
+        cin >> arr[i];
+    }
+    int s;
+    cout << "Enter element to be searched : ";
+    cin >> s;
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == s)
+        {
+            k = 1;
+            break;
+        }
+    }
+    if (k == 1)
+        cout << "Number is present";
+    else
+        cout << "Number is not present";
+}
