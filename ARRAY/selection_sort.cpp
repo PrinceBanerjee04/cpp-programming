596766c42e76537981675fbffc05c38613e69ed1
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-20 19:14:15 +0530

    Create selection_sort.cpp

---
+++ ARRAY/selection_sort.cpp
@@ -0,0 +1,36 @@
+int main()
+{
+    int n, i, j, min, temp;
+    cout << "Enter size : ";
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+    cout << "GIVEN ARRAY : ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<"\n";
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    cout << "SORTED ARRAY : ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
