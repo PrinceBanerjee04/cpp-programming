39e5a7d8c639e66138ac55b1699c60d87c4c8438
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-22 20:25:38 +0530

    Create triangular_number.cpp

---
+++ NUMBERS/triangular_number.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    int i, s = 3, a = 3, n;
+    cout << "ENTER N : ";
+    cin >> n;
+    if (n < 3)
+    {
+        cout << "INVALID INPUT";
+    }
+    else
+    {
+        cout << "TRIANGULAR NUMBERS ARE : " << endl;
+        for (i = 3; s <= n; i++)
+        {
+            cout << s << endl;
+            s = s + i;
+        }
+    }
+    return 0;
+}
