64044486011e7c196c6cbf2ffe9f7120c7453154
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-22 20:27:08 +0530

    Create unique.cpp

---
+++ NUMBERS/unique.cpp
@@ -0,0 +1,40 @@
+int main()
+{
+    int m, n;
+    cout<<"ENETR M : ";
+    cin >> m;
+    cout<<"ENTER N : ";
+    cin >> n;
+    if (m < 1 || m > 30000 || n < 1 || n > 30000 || m > n)
+    {
+        cout << "INVALID INPUT";
+    }
+    else
+    {
+        int c = 0, i, d, n1;
+        for (i = m; i <= n; i++)
+        {
+            n1 = i;
+            bool a[10]={false};
+            bool Unique = true;
+            while (n1 > 0)
+            {
+                d = n1 % 10;
+                if (a[d]==true)
+                {
+                    Unique = false;
+                    break;
+                }
+                a[d] = true;
+                n1 /= 10;
+            }
+            if (Unique==true)
+            {
+                c++;
+                cout<<i<<" ";
+            }
+        }
+        cout << "\n";
+        cout << "FREQUENCY OF UNIQUE DIGIT INTERGERS IS : " << c;
+    }
+}
