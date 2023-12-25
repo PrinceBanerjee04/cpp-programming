f902712ddc26f2252c376328af889bad33dda702
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-25 14:59:20 +0530

    Create heart.cpp

---
+++ PATTERN/heart.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+    int i,j,n;
+    cout<<"Enter Size : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n+1;j++)
+        {
+            if((i==0 && j%3!=0) || (i==1 && j%3==0) || (i-j==2) || (i+j==8))
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
