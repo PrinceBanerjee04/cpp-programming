1cd5b3587d3dfbe2846542ca0ad97bf3f350ffec
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-25 14:59:51 +0530

    Create floyd's_triangle.cpp

---
+++ PATTERN/floyd's_triangle.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int c=1;
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<c<<" ";
+            c++;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
