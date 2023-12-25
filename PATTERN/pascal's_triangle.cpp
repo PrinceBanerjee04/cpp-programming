7330d3b4e9cf426f9cbe09744c221b4a7c4172f5
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-25 15:00:31 +0530

    Create pascal's_triangle.cpp

---
+++ PATTERN/pascal's_triangle.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+    int n, i, j, k, l;
+    cout << "Enter size of the pattern : ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (k = 1; k <= (n - i); k++)
+        {
+            cout << "  ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        for (l = i - 1; l >= 1; l--)
+        {
+            cout << l << " ";
+        }
+        cout << endl;
+    }
+}
