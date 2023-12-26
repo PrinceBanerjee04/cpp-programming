d8b09b2b6717ceca9f37e0850ff72493c40a1408
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-26 12:02:15 +0530

    Create square.cpp

---
+++ PATTERN/square.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int n,i,j;
+    cout<<"Enter size : ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
