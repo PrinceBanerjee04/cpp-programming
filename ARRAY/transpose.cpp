a0a97661fd050434880973d6a9c38ce44fcae788
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2023-12-27 11:43:44 +0530

    Update transpose.cpp

--- ARRAY/transpose.cpp
+++ ARRAY/transpose.cpp
@@ -1,9 +1,35 @@
-cout<<"TRANSPOSE OF THE MATRIX : "<<endl;
-for(i=0;i<n;i++)
+int main()
 {
-    for(j=0;j<n;j++)
+    int n;
+    cout << "Enter array size :";
+    cin >> n;
+    int a[n][n], arr[n][n], i, j, k = 0;
+    for (i = 0; i < n; i++)
     {
-        cout<<a[j][i]<<" ";
+       
+            cout << "Enter element " << k << " : ";
+        for (j = 0; j < n; j++)
+        {
+            k++;
+            cin >> a[i][j];
+        }
     }
-    cout<<"\n";
+    cout << "ORIGINAL MATRIX" << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout<<"TRANSPOSE OF THE MATRIX : "<<endl;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<a[j][i]<<" ";
+        }
+        cout<<"\n";
+    }
 }
