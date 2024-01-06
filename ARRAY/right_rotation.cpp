f97bd0806ca0ea1185b44c6fd4d0515294043ba5
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-06 12:53:10 +0530

    Create right_rotation.cpp

---
+++ ARRAY/right_rotation.cpp
@@ -0,0 +1,28 @@
+int main()
+{
+    int n, d, i;
+    cout << "Enter array size : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+    cout << "Enter steps to be rotated : ";
+    cin >> d;
+    vector<int> temp(n);
+    for (int i = 0; i < n; i++)
+    {
+        temp[(i + d) % n] = arr[i];
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     arr[i] = temp[i];
+    // }
+    for (i = 0; i < n; i++)
+    {
+        cout << temp[i] << " ";
+    }
+    return 0;
+}
