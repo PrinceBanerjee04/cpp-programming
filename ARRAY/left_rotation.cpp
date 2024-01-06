b2cf105f41e8a3d1dacbb1c4c229f161cf2a1a8b
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-06 12:52:17 +0530

    Create left_rotation.cpp

---
+++ ARRAY/left_rotation.cpp
@@ -0,0 +1,33 @@
+int main()
+{
+    int n, d, i, k = 0;
+    cout << "Enter array size : ";
+    cin >> n;
+    int arr[n], temp[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+    cout << "Enter steps to be rotated : ";
+    cin >> d;
+    for (i = d; i < n; i++)
+    {
+        temp[k] = arr[i];
+        k++;
+    }
+    for (i = 0; i < d; i++)
+    {
+        temp[k] = arr[i];
+        k++;
+    }
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
