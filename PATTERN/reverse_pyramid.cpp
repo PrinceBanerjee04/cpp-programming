204aadc4b8f059b0a4dcb58bff5c925dcdea7453
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-07 21:06:26 +0530

    Create reverse_pyramid.cpp

---
+++ PATTERN/reverse_pyramid.cpp
@@ -0,0 +1,18 @@
+int main()
+{
+    int i, j, k, n;
+    cout << "Enter size : ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (k = 0; k < 2 * (n - i) - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
