f4f49f351ad5ab0079033be528fdfb0f81cbc9a8
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-07 21:04:48 +0530

    Create right_triangle.cpp

---
+++ PATTERN/right_triangle.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
