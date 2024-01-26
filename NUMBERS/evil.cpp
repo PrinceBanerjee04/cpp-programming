bd585b44e0591fd7b5f77c6374a92596dacba365
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-27 01:00:29 +0530

    Create evil.cpp

---
+++ NUMBERS/evil.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+    int n;
+    cout << "ENTER N : ";
+    cin >> n;
+    int c = 0, c1 = 0, bn = 0, d;
+    while (n > 0)
+    {
+        d = n % 2;
+        if (d == 1)
+            c++;
+        bn = bn + (int)(d * pow(10, c1));
+        c1++;
+        n /= 2;
+    }
+    cout << "BINARY EQUIVALENT : " << bn << endl;
+    cout << "No. of 1's : " << c << endl;
+    if (c % 2 == 0)
+        cout << "EVIL NUMBER";
+    else
+        cout << "NOT AN EVIL NUMBER";
+}
