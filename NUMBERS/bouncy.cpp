c86ffae46d32669a3ff0374853c8505e02d5330a
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-27 01:01:04 +0530

    Create bouncy.cpp

---
+++ NUMBERS/bouncy.cpp
@@ -0,0 +1,45 @@
+int main()
+{
+    int n;
+    cout << "ENTER N : ";
+    cin >> n;
+    if (n < 100)
+    {
+        cout << "<<IVALID INPUT";
+    }
+    else
+    {
+        int n1 = n, d, d1, c = 0, c1 = 0;
+        d1 = n1 % 10;
+        while (n1 > 0)
+        {
+            d = n1 % 10;
+            if (d > d1)
+            {
+                c = 1;
+                break;
+            }
+            d1 = d;
+            n1 /= 10;
+        }
+        n1 = n;
+        d1 = n1 % 10;
+        while (n1 > 0)
+        {
+            d = n1 % 10;
+            if (d < d1)
+            {
+                c1 = 1;
+                break;
+            }
+            d1 = d;
+            n1 /= 10;
+        }
+        if (c == 1 && c1 == 1)
+        {
+            cout << n << " is a Bouncy Number";
+        }
+        else
+            cout << n << " is not a Bouncy Number";
+    }
+}
