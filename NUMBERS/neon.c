ab32f207651e2642dc751341244e2ddceb495299
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-07 13:45:23 +0530

    Create neon.c

---
+++ NUMBERS/neon.c
@@ -0,0 +1,18 @@
+int main()
+{
+    cout<<"Enter a number : ";
+    int n,sq,s=0,d;
+    cin>>n;
+    sq=n*n;
+    while(sq>0)
+    {
+        d=sq%10;
+        s=s+d;
+        sq/=10;
+    }
+    if(n==s)
+    cout<<"Neon Number";
+    else
+    cout<<"Not a Neon Number";
+    return 0;
+}
