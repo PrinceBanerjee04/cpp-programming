e8cf0facea3cff66f08d0357573d32191c29f809
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-07 13:46:56 +0530

    Create spy.c

---
+++ NUMBERS/spy.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int n,s=0,p=1,d;
+    cout<<"Enter a number : ";
+    cin>>n;
+    while(n>0)
+    {
+        d=n%10;
+        s=s+d;
+        p=p*d;
+        n/=10;
+    }
+    if(s==p)
+    cout<<"SPY NUMBER";
+    else
+    cout<<"NOT A SPY NUMBER";
+}
