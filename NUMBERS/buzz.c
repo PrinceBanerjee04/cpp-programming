278e4e53a12a590cad92e0357df0dc720d62acf6
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-08 21:45:10 +0530

    Update buzz.c

--- NUMBERS/buzz.c
+++ NUMBERS/buzz.c
@@ -1,1 +1,11 @@
-.
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    if (n % 7 == 0 && n % 7 == 0)
+        cout << "BUZZ NUMBER ";
+    else
+        cout << "NOT A BUZZ NUMBER";
+    return 0;
+}
