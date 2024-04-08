c8a847ec0e933968fa8756555d923c2caf4718c8
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-08 21:43:53 +0530

    Update strontio.c

--- NUMBERS/strontio.c
+++ NUMBERS/strontio.c
@@ -1,1 +1,26 @@
-.
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int n1 = n, d, c = 0;
+    while (n1 > 0)
+    {
+        d = n1 % 10;
+        c++;
+        n1 /= 10;
+    }
+    if (c < 4)
+    {
+        cout << "INVALID INPUT";
+    }
+    else
+    {
+        n1 = (n1 * 2 % 1000) / 10;
+        if (n1 % 10 == n1 / 10)
+            cout << "STRONTIO NUMBER";
+        else
+            cout << "NOT A STRONTIO NUMBER";
+    }
+    return 0;
+}
