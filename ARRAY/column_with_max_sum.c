0efc386f73e66fc3460830f7e3a9ea55e2f53b3a
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-10 22:02:23 +0530

    Update column_with_max_sum.c

--- ARRAY/column_with_max_sum.c
+++ ARRAY/column_with_max_sum.c
@@ -1,4 +1,29 @@
-    int r = 0, max = 0;
+int main()
+{
+    int n;
+    cout << "Enter array size :";
+    cin >> n;
+    int a[n][n], arr[n][n], i, j, k = 1;
+    for (i = 0; i < n; i++)
+    {
+            //cout << "Enter element " << k << " : ";
+        for (j = 0; j < n; j++)
+        {
+            k++;
+            cin >> a[i][j];
+        }
+    }
+    cout << "ORIGINAL MATRIX" << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+int r = 0, max = 0;
     for (i = 0; i < n; i++)
     {
         r = 0;
