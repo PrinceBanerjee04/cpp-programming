3d22f5f8715e06511266287bc413533611b045d8
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-10 22:01:40 +0530

    Update column_with_max_sum.c

--- ARRAY/column_with_max_sum.c
+++ ARRAY/column_with_max_sum.c
@@ -1,1 +1,13 @@
-.
+    int r = 0, max = 0;
+    for (i = 0; i < n; i++)
+    {
+        r = 0;
+        for (j = 0; j < n; j++)
+        {
+            r = r + a[i][j];
+        }
+        if (r > max)
+        {
+            max = r;
+        }
+    }
