c12b039b83fcd5dc84922e30746f376d211d5007
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-13 12:53:10 +0530

    Update daabang.cpp

--- PATTERN/daabang.cpp
+++ PATTERN/daabang.cpp
@@ -1,1 +1,26 @@
-.
+int main()
+{
+    int n, i, j, k, l;
+    cout << "Enter size of the pattern :- ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= (n - i + 1); j++)
+        {
+            cout << j << " ";
+        }
+        for (k = 1; k <= (i - 1); k++)
+        {
+            cout << "* ";
+        }
+        for (k = (i - 1); k >= 1; k--)
+        {
+            cout << "* ";
+        }
+        for (l = (n - i + 1); l >= 1; l--)
+        {
+            cout << l << " ";
+        }
+        cout << endl;
+    }
+}
