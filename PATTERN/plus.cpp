d8de9804a3b4d1535a1cf6c2e86b940cdfb01554
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-13 12:51:59 +0530

    Update plus.cpp

--- PATTERN/plus.cpp
+++ PATTERN/plus.cpp
@@ -1,1 +1,20 @@
-,
+int main()
+{
+    int i,j,k,n;
+    cout<<"Enter size : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==n/2)
+            cout<<"*";
+            else if(j==n/2)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
