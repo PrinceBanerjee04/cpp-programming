4b11df6822e273490f5857f516fee9926e0495d9
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-13 12:54:12 +0530

    Update cross.cpp

--- PATTERN/cross.cpp
+++ PATTERN/cross.cpp
@@ -1,1 +1,17 @@
-.
+int  main()
+{
+    int i,j,n;
+    cout<<"Enter size : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j || i+j==n-1)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+}
