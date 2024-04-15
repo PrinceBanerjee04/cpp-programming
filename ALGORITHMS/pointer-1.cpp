a1a8a4e652137d1bd6af01b7eab2353aec373c6c
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-15 22:39:37 +0530

    Update pointer-1.cpp

--- ALGORITHMS/pointer-1.cpp
+++ ALGORITHMS/pointer-1.cpp
@@ -1,1 +1,24 @@
-.
+int main()
+{
+    int num = 50;
+    int *ptr = &num;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    cout << "\n";
+
+    cout << "size of integer is " << sizeof(num) << endl;
+    cout << "size of pointer is " << sizeof(*ptr) << endl;
+
+    cout << "\n";
+
+    cout << "before " << num << endl;
+    (*ptr)++;
+    cout << "after " << num << endl;
+
+    cout << "\n";
+
+    cout << "before p " << ptr << endl;
+    ptr++;
+    cout << "after p " << ptr << endl;
+}
