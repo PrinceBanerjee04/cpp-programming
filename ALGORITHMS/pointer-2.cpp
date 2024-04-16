24b12c3034bce1bdc8155ad6fabac93cb9cd2cb3
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-16 19:30:12 +0530

    Update pointer-2.cpp

--- ALGORITHMS/pointer-2.cpp
+++ ALGORITHMS/pointer-2.cpp
@@ -1,1 +1,38 @@
-.
+int main()
+{
+    int arr[4] = {23, 122, 41, 67};
+
+    cout << "address of a memory block is " << arr << endl;
+    cout << "address of a memory block is " << &arr << endl;
+    cout << "address of a memory block is " << &arr[0] << endl;
+
+    cout << "\n";
+
+    cout << "1st element " << arr[0] << endl;
+    cout << "1st element " << *arr << endl;
+    cout << "2nd Element " << *(arr + 1) << endl;
+
+    cout << "\n";
+
+    cout << "Adds 1 to the 1st element " << *arr + 1 << endl;
+    cout << "Adds 1 to the 1st element" << *(arr) + 1 << endl;
+
+    cout << "\n";
+
+    cout << "3rd element " << arr[2] << endl;
+    cout << "3rd element " << *(arr + 2) << endl;
+
+    cout << "\n";
+
+    int *p = &arr[0];
+    cout << p << endl;
+    cout << *p << endl;
+    cout << "adress of pointer-p " << &p << endl;
+    cout << "\n";
+
+    char ch[6] = {'a', 'b', 'c', 'd', 'e'};
+    cout << ch << endl;
+
+    char *c = &ch[0];
+    cout << c << endl;
+}
