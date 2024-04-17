1d8b1f590b5700cb0f58f99fec612a4b3e0ed69b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-17 19:24:10 +0530

    Update pointer-3.cpp

--- ALGORITHMS/pointer-3.cpp
+++ ALGORITHMS/pointer-3.cpp
@@ -1,1 +1,21 @@
+int main()
+{
+    int i = 5;
+    int *p = &i;
+    int **p2 = &p;
 
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
+
+    cout << "\n";
+
+    cout << &i << endl;
+    cout << p << endl;
+    cout << *p2 << endl;
+
+    cout << "\n";
+
+    cout << &p << endl;
+    cout << p2 << endl;
+}
