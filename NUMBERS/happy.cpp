3a9a4b0fe0b4f98be781428be91ffc197602b2d2
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-19 23:12:53 +0530

    Update happy.cpp

--- NUMBERS/happy.cpp
+++ NUMBERS/happy.cpp
@@ -1,1 +1,23 @@
-
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int d, s = 0;
+    while (n > 9)
+    {
+        while (n > 0)
+        {
+            d = n % 10;
+            s = s + (d * d);
+            n /= 10;
+        }
+        n = s;
+        s = 0;
+    }
+    if (n == 1)
+        cout << "HAPPY NUMBER";
+    else
+        cout << "NOT A HAPPY NUMBER";
+    return 0;
+}
