0373098b5e0c165995d3c7e802f92a76381a6fba
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-20 15:46:18 +0530

    Update single_linked_list.cpp

--- ALGORITHMS/single_linked_list.cpp
+++ ALGORITHMS/single_linked_list.cpp
@@ -12,3 +12,25 @@ void printlist(Node *n)
         n = n->next;
     }
 }
+int main()
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    printlist(head);
+    return 0;
+}
