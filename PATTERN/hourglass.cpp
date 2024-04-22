51bc82d53f9a35c6ab6343df293f95842a360b38
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-22 12:59:58 +0530

    Update hourglass.cpp

--- PATTERN/hourglass.cpp
+++ PATTERN/hourglass.cpp
@@ -1,1 +1,23 @@
-
+int main()
+{
+    int i, n, j, k;
+    cout << "Enter size : ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+            cout << " ";
+        for (int k = 0; k < (n - i) * 2 - 1; k++)
+            cout << "*";
+        cout << "\n";
+    }
+    for (i = 2; i <= n; i++)
+    {
+        for (j = n; j > i; j--)
+            cout << " ";
+        for (k = 0; k < i * 2 - 1; k++)
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
