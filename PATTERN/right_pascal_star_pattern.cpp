ee8fa0029c98b995a68e77fbb878d1de57448411
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-24 01:23:13 +0530

    Update right_pascal_star_pattern.cpp

--- PATTERN/right_pascal_star_pattern.cpp
+++ PATTERN/right_pascal_star_pattern.cpp
@@ -1,1 +1,23 @@
-
+int main()
+{
+    int n, i, j;
+    cout << "Enter size : ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (i = 1; i <= n - 1; i++)
+    {
+        for (j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
