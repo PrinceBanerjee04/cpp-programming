e52562feee071f7c705883591370b25b5190f4cb
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-04-30 12:14:28 +0530

    Update magic.cpp

--- NUMBERS/magic.cpp
+++ NUMBERS/magic.cpp
@@ -1,1 +1,23 @@
-
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int d,s=0;
+    while (n > 9)
+    {
+        while (n > 0)
+        {
+            d = n % 10;
+            s = s + d;
+            n /= 10;
+        }
+        n = s;
+        s = 0;
+    }
+    if (n == 1)
+        cout << "MAGIC NUMBER";
+    else
+        cout << "NOT A MAGIC NUMBER";
+    return 0;
+}
