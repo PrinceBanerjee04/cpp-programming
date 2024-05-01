28dce0b0cde44d25581e275176718e835852ad72
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-01 15:23:06 +0530

    Update kaprekar.cpp

--- NUMBERS/kaprekar.cpp
+++ NUMBERS/kaprekar.cpp
@@ -1,1 +1,55 @@
-
+int main()
+{
+    int i, p, q, n, n1 = 0, n2 = 0, num1, d, d1, p1 = 0, k = 0, w, m = 0, r = 0, c = 0;
+    cout << "ENTER P : ";
+    cin >> p;
+    cout << "ENTER Q : ";
+    cin >> q;
+    cout << "Kaprekar Numbers between " << p << " and " << q << " are = ";
+    for (n = p; n <= q; n++)
+    {
+        k = 0;
+        m = 0;
+        r = 0;
+        n1 = 0;
+        n2 = 0;
+        p1 = 0;
+        num1 = n * n;
+        w = n * n;
+        while (w != 0)
+        {
+            d = w % 10;
+            k++;
+            w = w / 10;
+        }
+        if (k % 2 == 0)
+            p1 = k / 2;
+        else
+            p1 = k / 2 + 1;
+        for (i = 1; i <= k; i++)
+        {
+            if (i <= p1)
+            {
+                d = num1 % 10;
+                n1 = n1 + (int)(d * pow(10, m));
+                num1 = num1 / 10;
+                m++;
+            }
+            else
+            {
+                d1 = num1 % 10;
+                n2 = n2 + (int)(d1 * pow(10, r));
+                num1 = num1 / 10;
+                r++;
+            }
+        }
+        if (n1 + n2 == n)
+        {
+            cout << n << " ";
+            c++;
+        }
+    }
+    cout << "\n";
+    cout << "TOTAL NUMBER OF KAPREKAR NUMBERS = " << c;
+    return 0;
+}
