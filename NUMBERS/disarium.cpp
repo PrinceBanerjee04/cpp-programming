1cbe6227b01a1d698425132e99c026c2529a2341
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-01 15:21:29 +0530

    Update disarium.cpp

--- NUMBERS/disarium.cpp
+++ NUMBERS/disarium.cpp
@@ -1,1 +1,25 @@
-
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int n1 = n, c = 0, d, s = 0;
+    while (n1 > 0)
+    {
+        n1 /= 10;
+        c++;
+    }
+    n1 = n;
+    while (n1 > 0)
+    {
+        d = n1 % 10;
+        s = s +(int) pow(d, c);
+        c--;
+        n1 /= 10;
+    }
+    if (n == s)
+        cout << "DISARIUM NUMBER";
+    else
+        cout << "NOT A DISARIUM NUMBER";
+    return 0;
+}
