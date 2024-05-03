1a8497dbdba464615b3e4ceb7bdde245e3ce41e5
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-03 19:52:46 +0530

    Update left_pascal_star.cpp

--- PATTERN/left_pascal_star.cpp
+++ PATTERN/left_pascal_star.cpp
@@ -1,1 +1,27 @@
-
+int main()
+{
+    int n, i, j, k;
+    cout<<"ENTER N : ";
+    cin>>n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (k = 0; k < i; k++)
+            cout << "*";
+        cout << "\n";
+    }
+    for (i = 1; i <= n - 1; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (k = 0; k < n - i; k++)
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
