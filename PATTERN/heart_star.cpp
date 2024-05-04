5dd4de4cc1f842224cb9a29fd0241a1b1896836b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-04 15:08:35 +0530

    Update heart_star.cpp

--- PATTERN/heart_star.cpp
+++ PATTERN/heart_star.cpp
@@ -1,1 +1,25 @@
-
+int main()
+{
+    int n, i, j;
+    cout << "ENTER N :";
+    cin >> n;
+    for (i = n / 2; i < n; i += 2)
+    {
+        for (j = 1; j < n - i; j += 2)
+        {
+            cout << " ";
+        }
+        for (j = 1; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (j = 1; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        for (j = 1; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
