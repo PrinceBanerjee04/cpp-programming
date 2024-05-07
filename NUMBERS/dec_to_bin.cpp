cf1e6a460906f335d4e94fc1b6b3a87fb2f27f38
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-07 15:12:31 +0530

    Update dec_to_bin.cpp

--- NUMBERS/dec_to_bin.cpp
+++ NUMBERS/dec_to_bin.cpp
@@ -1,1 +1,14 @@
-
+int main()
+{
+    int n,d;
+    string st="";
+    cout<<"Enter a number : ";
+    cin>>n;
+    while(n>0)
+    {
+        d=n%2;
+        st=to_string(d)+st;
+        n/=2;
+    }
+    cout<<"Binary Equivalent : "<<st;
+}
