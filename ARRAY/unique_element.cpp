9385d44b57d9cf16915a37a9142c8808f3c0134f
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-08 19:58:19 +0530

    Update unique_element.cpp

--- ARRAY/unique_element.cpp
+++ ARRAY/unique_element.cpp
@@ -1,1 +1,27 @@
-
+int main()
+{
+    int n;
+    cout << "Enter array size : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << (i + 1) << " : ";
+        cin >> arr[i];
+    }
+    cout << "GIVEN ARRAY : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    if (ans == 0)
+        cout << "No unique element is present";
+    else
+        cout << "Unique Element is : " << ans;
+}
