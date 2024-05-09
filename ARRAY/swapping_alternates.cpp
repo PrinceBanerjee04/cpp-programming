fde5185f3538557de58d3c7f61ce3cd4c19748bd
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-09 19:01:26 +0530

    Update swapping_alternates.cpp

--- ARRAY/swapping_alternates.cpp
+++ ARRAY/swapping_alternates.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int arr[6]={1,2,3,4,5,6};
+    int i;
+    for(i=0;i<6;i+=2)
+    {
+        if(i+1<6)
+        swap(arr[i],arr[i+1]);
+    }
+    for(i=0;i<6;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
