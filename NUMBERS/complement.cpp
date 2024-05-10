2f5c28ce461cc8e39f144540d6d9ed8ca2dfdc63
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-11 00:42:40 +0530

    Update complement.cpp

--- NUMBERS/complement.cpp
+++ NUMBERS/complement.cpp
@@ -1,1 +1,16 @@
-
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    if (n == 0)
+        return 1;
+    int d = 0, n1 = n;
+    while (n1 > 0)
+    {
+        d = (d << 1) | 1;
+        n1 = n1 >> 1;
+    }
+    int r = (~n) & d;
+    cout<<r;
+}
