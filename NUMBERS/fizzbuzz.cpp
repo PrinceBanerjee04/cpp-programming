3cd5f78e9069bb18370831a6be4985e372fdec26
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-05-11 00:33:59 +0530

    Update fizzbuzz.cpp

--- NUMBERS/fizzbuzz.cpp
+++ NUMBERS/fizzbuzz.cpp
@@ -1,1 +1,22 @@
-
+int main()
+{
+    int n;
+    cout << "Enter a number :";
+    cin >> n;
+    if (n % 3 == 0 && n % 5 == 0)
+    {
+        cout << n << " IS A FIZZBUZZ NUMBER " << endl;
+    }
+    else if (n % 3 == 0)
+    {
+        cout << n <<" IS A FIZZ NUMBER" << endl;
+    }
+    else if (n % 5 == 0)
+    {
+        cout << n << " IS A BUZZ NUMBER" << endl;
+    }
+    else if (n % 3 != 0 || n % 5 != 0)
+    {
+        cout << n << " IS NOT A FIZZ BUZZ NUMBER" << endl;
+    }
+}
