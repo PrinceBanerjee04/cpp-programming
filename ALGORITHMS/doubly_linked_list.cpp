c7702ea68c45fe9a30690cf78b3aa8ab18227faf
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-07-26 19:06:26 +0530

    Update doubly_linked_list.cpp

--- ALGORITHMS/doubly_linked_list.cpp
+++ ALGORITHMS/doubly_linked_list.cpp
@@ -5,3 +5,16 @@ public:
     Node *next;
     Node *prev;
 };
+void push(Node **head_ref, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+
+    (*head_ref) = new_node;
+}
